95ea166dd75de1bbb5e7e2f9669ed1f44ef2f9b6
2mxujhyt <70924685+2mxujhyt@users.noreply.github.com>
2025-11-27 00:41:55 -0600

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+  var TITLE = "Social Democracy: A Minnesotan History" + '_' + "Autumn Chen (mod by 2mxujhyt)";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
